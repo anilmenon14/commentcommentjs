57a0083334b1562acae9be26baac9370d65ffeec
anilmenon14 <anil.menon14@gmail.com>
2020-04-25 22:59:12 +0400

    Updated Post.js

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import Post from './Post'
+
+function App() {
+  return (
+    <div className="App">
+
+<Post />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Post.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+class Post extends React.Component {
+
+
+render() {
+
+return "CommentPost Component"
+
+}
+
+
+
+
+
+
+}
+
+
+export default Post;
