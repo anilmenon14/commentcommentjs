3d16ba136fdc6f58ed9c0f16044e68d6e98cdb17
anilmenon14 <anil.menon14@gmail.com>
2020-05-08 23:02:44 +0400

    Header changes

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -3,6 +3,7 @@ import React from "react";
 const Comments = () => {
   return (
     <div className="comments-area">
+      <h3>Comments:</h3>
       This is where the comments will be
     </div>
   );
--- src/components/RightPane.js
+++ src/components/RightPane.js
@@ -6,8 +6,23 @@ const RightPane = () => {
   return (
     <main id="right" className="page-content">
       <div className="page-header">
-        <div>CommentCommentJS</div>
-        <div>Right aligned icons</div>
+        <div>
+          Comment<br></br>
+          <span className="tab" />
+          <span className="tab" />
+          <img className="img1" src="/src/img/reply-icon.png" alt=""/>
+          <span className="tab" />
+          Comment<br></br>
+          <span className="tab" />
+          <span className="tab" />
+          <span className="tab" />
+          <span className="tab" />
+          <span className="tab" />
+          <span className="tab" />
+
+          JS
+        </div>
+        <div>Information</div>
       </div>
       <div className="main-post-area">
         <Post />
