c0f515bd86b134a3c889da34f7b42444c61dcdcb
anilmenon14 <anil.menon14@gmail.com>
2020-05-13 22:31:03 +0400

    Comments header icons and layout

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -3,9 +3,47 @@ import React from "react";
 const Comments = () => {
   return (
     <div className="comments-area">
-      <h3>Comments:</h3>
-      This is where the comments will be.
-      posuere maximus. Suspendisse potenti. Duis eu massa eu purus lobortis tempus vitae id quam. Curabitur ac dolor cursus, tempor turpis non, tincidunt est. Morbi nunc nunc, commodo at finibus eget, faucibus id purus. Integer tempor molestie tellus vel consequat. Sed ut nulla consectetur, accumsan diam id, tincidunt ipsum. Duis molestie fringilla purus quis vehicula. Fusce dolor felis, vehicula ut faucibus id, posuere at nisi. Fusce aliquam dignissim varius. Suspendisse potenti. Nulla non quam ac eros rhoncus tincidunt. Integer euismod erat id mi pharetra maximus. Duis at metus euismod, consequat lacus vel, malesuada enim. Curabitur nisi nisl, fermentum et facilisis sit amet, feugiat non nisi. Ut auctor nulla nec rhoncus volutpat. Vivamus eu risus turpis. Nulla nibh felis, fermentum quis turpis at, hendrerit bibendum tortor. Ut et nunc vel turpis faucibus bibendum. Aliquam mattis, mauris et eleifend elementum, lacus enim aliquam elit, eget tristique justo nulla ac felis. Phasellus sit amet orci arcu. Vivamus sed laoreet erat. Sed bibendum bibendum ante vel porta. In hac habitasse platea dictumst. Fusce ex risus, mollis ultrices placerat at, ornare at nisi. Sed dictum felis ut justo pretium convallis. Cras in hendrerit justo, et molestie nisl.
+      <div className="comments-header">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="36"
+          viewBox="0 0 24 24"
+          width="36"
+        >
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path d="M3 18h12v-2H3v2zM3 6v2h18V6H3zm0 7h18v-2H3v2z" />
+        </svg>
+        <h3>Comments:</h3>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="36"
+          viewBox="0 0 24 24"
+          width="36"
+          className="testing123"
+        >
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path d="M21.99 4c0-1.1-.89-2-1.99-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4-.01-18zM17 11h-4v4h-2v-4H7V9h4V5h2v4h4v2z" />
+        </svg>
+      </div>
+      This is where the comments will be. posuere maximus. Suspendisse potenti.
+      Duis eu massa eu purus lobortis tempus vitae id quam. Curabitur ac dolor
+      cursus, tempor turpis non, tincidunt est. Morbi nunc nunc, commodo at
+      finibus eget, faucibus id purus. Integer tempor molestie tellus vel
+      consequat. Sed ut nulla consectetur, accumsan diam id, tincidunt ipsum.
+      Duis molestie fringilla purus quis vehicula. Fusce dolor felis, vehicula
+      ut faucibus id, posuere at nisi. Fusce aliquam dignissim varius.
+      Suspendisse potenti. Nulla non quam ac eros rhoncus tincidunt. Integer
+      euismod erat id mi pharetra maximus. Duis at metus euismod, consequat
+      lacus vel, malesuada enim. Curabitur nisi nisl, fermentum et facilisis sit
+      amet, feugiat non nisi. Ut auctor nulla nec rhoncus volutpat. Vivamus eu
+      risus turpis. Nulla nibh felis, fermentum quis turpis at, hendrerit
+      bibendum tortor. Ut et nunc vel turpis faucibus bibendum. Aliquam mattis,
+      mauris et eleifend elementum, lacus enim aliquam elit, eget tristique
+      justo nulla ac felis. Phasellus sit amet orci arcu. Vivamus sed laoreet
+      erat. Sed bibendum bibendum ante vel porta. In hac habitasse platea
+      dictumst. Fusce ex risus, mollis ultrices placerat at, ornare at nisi. Sed
+      dictum felis ut justo pretium convallis. Cras in hendrerit justo, et
+      molestie nisl.
     </div>
   );
 };
